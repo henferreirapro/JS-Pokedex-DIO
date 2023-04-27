d3de1c11087f531e5d1e30ea554556d47da845fc
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-04-27 12:06:48 -0300

    Criando Requisições  Detalhes Pokemon.
    Criei novas requisições para acessar a url dos detalhes do pokemon e me retornar os detalhes alem do nome, como número, tipo, imagens, etc...

--- assets/js/poke.api.js
+++ assets/js/poke.api.js
@@ -1,10 +1,18 @@
 const pokeApi = {};
 
+pokeApi.getDetalhesPokemons = (pokemon) => {
+  return fetch(pokemon.url).then((respose) => respose.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
   return fetch(url) 
     .then((res) => res.json())
     .then((jsonBody) => jsonBody.results)
+    .then((listaPokemonsUrl) => listaPokemonsUrl.map(pokeApi.getDetalhesPokemons))
+    .then((resquestDetalhes) => Promise.all(resquestDetalhes))
+    .then((pokemonDetalhes) => pokemonDetalhes)
+
     .catch((error) => console.error(error))
     
 }
